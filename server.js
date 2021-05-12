a69cae959454ab83a993b8ce5762efec02bd4a0d
arvind <arvind9126@hotmail.com>
2021-05-12 15:42:32 +0530

    add dist

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/local-storage-master'));
+app.use(express.static('./dist/local-storage'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/local-storage-master/'}),
+    res.sendFile('index.html', {root: 'dist/local-storage/'}),
 );
 
 // Start the app by listening on the default Heroku port
